cc65a448afbb7adbcda29ae023e366e57db6c613
DEXTER <144078621+purnapurna2007@users.noreply.github.com>
2024-05-01 04:13:22 +0530

    Add files via upload

--- pair.js
+++ pair.js
@@ -6,8 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Gifted_Tech,
-    useMultiFileAuthState,
+    default: Venocyber_Tech,    useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
     Browsers
@@ -20,13 +19,13 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function GIFTED_MD_PAIR_CODE() {
+        async function UNIQUE_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Pair_Code_By_Gifted_Tech = Gifted_Tech({
+            let Pair_Code_By_Venocyber_Tech = Venocyber_Tech({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
@@ -35,16 +34,16 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: ["Chrome (Linux)", "", ""]
              });
-             if(!Pair_Code_By_Gifted_Tech.authState.creds.registered) {
+             if(!Pair_Code_By_Venocyber_Tech.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Gifted_Tech.requestPairingCode(num)
+                            const code = await Pair_Code_By_Venocyber_Tech.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Gifted_Tech.ev.on("connection.update", async (s) => {
+            Pair_Code_By_Venocyber_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Venocyber_Tech.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
@@ -54,42 +53,37 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_Venocyber_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id, { text: '' + b64data });
 
-               let GIFTED_MD_TEXT = `
-
-*✅sᴇssɪᴏɴ ᴄᴏɴɴᴇᴄᴛᴇᴅ✅*
-*Made With 💜*
-*By ᴅᴇxᴛᴇʀ💜*
-____________________________________
+               let UNIQUE_MD_TEXT = `
+*Pair Code Connected by DEXTER MD*
+*_Made With Dexter*
+______________________________________
 ╔════◇
-║『 𝐖𝐎𝐖 𝐘𝐎𝐔'𝐕𝐄 𝐂𝐇𝐎𝐒𝐄𝐍 𝐃𝐄𝐗𝐓𝐄𝐑 𝐌𝐃 』
-║ You've Completed the First Step
-║ to Deploy a Whatsapp Bot.
-╚════════════════════╝
+║ *『 WELCOME  』*
+║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+╚══════════════════════╝
 ╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: https://youtube.com/@Dextertoola999?
-║❒ 𝐎𝐰𝐧𝐞𝐫: https://wa.me/message/LLGU3TSNGKH6J1
-║❒ 𝐑𝐞𝐩𝐨: _https://github.com/DEXTER-BOT-1/DEXTER-V1-MD_
-║❒ 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: _hhttps://chat.whatsapp.com/HjTJEJm6IX14AxUCsqfOQJ_
-║❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029VaOoODA65yDHLCqskX11_
-║❒ 𝐏𝐥𝐮𝐠𝐢𝐧𝐬: 📌  *Coming Soon*
-║    💜💜💜
-╚════════════════════╝ 
-*©²⁰²⁴ ᴰᴱˣᵀᴱᴿ ᵂᴴᴬᵀˢᴬᴾᴾ ᴮᴼᵀˢ*
-___________________________________
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Ytube:* _https://youtube.com/@Dextertoola999_
+║❒ *Owner:* _https://wa.me/message/LLGU3TSNGKH6J1_
+║❒ *Repo:* _https://github.com/DEXTER-BOT-1/DEXTER-V1-MD_
+║❒ *WaGroup:* _https://chat.whatsapp.com/HjTJEJm6IX14AxUCsqfOQJ_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaZZclvLtOjKGav75L1Bj_
+║❒_
+╚══════════════════════╝ 
+_____________________________________
 
-Don't Forget To Give Star⭐ To My Repo
- await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
+_DEXTER MD_`
+ await Pair_Code_By_Venocyber_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id,{text:UNIQUE_MD_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_Gifted_Tech.ws.close();
+        await Pair_Code_By_Venocyber_Tech.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    GIFTED_MD_PAIR_CODE();
+                    UNIQUE_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -100,6 +94,6 @@ Don't Forget To Give Star⭐ To My Repo
          }
         }
     }
-    return await GIFTED_MD_PAIR_CODE()
+    return await UNIQUE_MD_PAIR_CODE()
 });
 module.exports = router
